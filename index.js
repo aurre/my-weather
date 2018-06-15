5589d5ffb1f6f3e34bc6dab7d1213535412c809f
aurre <aurrecochearaysa@gmail.com>
2018-06-14 20:10:53 -0500

    retriving correctly from api

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-'use strict';
+#!/usr/bin/env node
 
 const axios = require('axios');
-const Conf = require('conf');
+const apiKey = require('./secret');
+
+const API_URL_CITY = 'http://api.openweathermap.org/data/2.5/weather?q=';
+
+const toFahrenheit = temp => Math.round((9 / 5) * (temp - 273) + 32);
+const toCelsius = temp => Math.round((temp - 32) * 0.5556);
+let flags = process.argv;
+flags = flags.slice(-1).toString();
+
+function getWeather(flags) {
+  let city = flags;
+  if (!city) {
+    return new Error('City missing!');
+  }
+
+  axios.get(`${API_URL_CITY}${city},us&appid=${apiKey}`).then(res => {
+    let temp = toFahrenheit(res.data.main.temp);
+    let response = Object.assign({
+      city: res.data.name,
+      country: res.data.sys.country,
+      Condition: res.data.weather[0].description,
+      Temperature: temp + ' F',
+    });
+    console.log(response);
+    return response;
+  });
+}
+getWeather(flags);
