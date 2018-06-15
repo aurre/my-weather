1c843d32c6795b492bffd3ce51929610b2e6c018
aurre <aurrecochearaysa@gmail.com>
2018-06-15 13:07:12 -0500

    new file created

---
+++ cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const weather = require('./index.js');
+
+program
+  .arguments('<input>')
+  .option('-c, --city <city>', 'City you want to lookup weather for')
+  .option('-C, --country <country>', 'Country you want to lookup weather for')
+  .option(
+    '-cel, --celsius<celsius>',
+    'Convert temp in celsius. Default to Fahrenheit'
+  )
+  .action(function(file) {
+    console.log(
+      'city: %s country: %s file: %s',
+      program.city,
+      program.country,
+      file
+    );
+  })
+  .parse(process.argv);
+
+if (program.city) {
+  let cityNumber = process.argv.indexOf(program.city);
+
+  weather.getWeather(process.argv[cityNumber]);
+}
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const axios = require('axios');
 const apiKey = require('./secret');
 
@@ -7,25 +5,33 @@ const API_URL_CITY = 'http://api.openweathermap.org/data/2.5/weather?q=';
 
 const toFahrenheit = temp => Math.round((9 / 5) * (temp - 273) + 32);
 const toCelsius = temp => Math.round((temp - 32) * 0.5556);
-let flags = process.argv;
-flags = flags.slice(-1).toString();
+// let flags = process.argv;
+// flags = flags.slice(-1).toString();
 
-function getWeather(flags) {
-  let city = flags;
-  if (!city) {
-    return new Error('City missing!');
-  }
+// console.log(process.argv);
 
-  axios.get(`${API_URL_CITY}${city},us&appid=${apiKey}`).then(res => {
-    let temp = toFahrenheit(res.data.main.temp);
-    let response = Object.assign({
-      city: res.data.name,
-      country: res.data.sys.country,
-      Condition: res.data.weather[0].description,
-      Temperature: temp + ' F',
-    });
-    console.log(response);
-    return response;
-  });
-}
-getWeather(flags);
+module.exports = {
+  getWeather: flags => {
+    let city = flags;
+    if (!city) {
+      return new Error('City missing!');
+    }
+
+    axios
+      .get(`${API_URL_CITY}${city},us&appid=${apiKey}`)
+      .then(res => {
+        let temp = toFahrenheit(res.data.main.temp);
+        let response = Object.assign({
+          city: res.data.name,
+          country: res.data.sys.country,
+          Condition: res.data.weather[0].description,
+          Temperature: temp + ' F',
+        });
+        console.log(response);
+        return response;
+      })
+      .catch(error => {
+        console.log('Sorry', error.response.data.message);
+      });
+  },
+};
