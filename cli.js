14f5bcbb29743e79f04159ba779e809742c76b2a
aurre <aurrecochearaysa@gmail.com>
2018-06-17 09:21:11 -0500

    lates chages

--- cli.js
+++ cli.js
@@ -2,29 +2,72 @@
 
 const program = require('commander');
 const weather = require('./index.js');
+const chalk = require('chalk');
 
 program
+  .version('0.1.0')
   .arguments('<input>')
   .option('-c, --city <city>', 'City you want to lookup weather for')
   .option('-C, --country <country>', 'Country you want to lookup weather for')
   .option(
-    '-cel, --celsius<celsius>',
+    '-s, --scale <scale>',
     'Convert temp in celsius. Default to Fahrenheit'
+  )
+  .option(
+    '-f, --forecast [value]',
+    'Optional if you want to see the weather for the next 5 days'
   )
   .action(function(file) {
     console.log(
-      'city: %s country: %s file: %s',
+      'city: %s country: %s, scale: %s file: %s',
       program.city,
       program.country,
+      program.scale,
       file
     );
   })
+
+  .on('option:verbose', function() {
+    process.env.VERBOSE = this.verbose;
+  })
+
+  // error on unknown commands
+
+  .on('command:*', function() {
+    console.error(
+      chalk.red(
+        'Invalid command: %s\nSee --help for a list of available commands.'
+      ),
+      program.args.join(' ')
+    );
+    process.exit(1);
+  })
   .parse(process.argv);
 
-if (program.city) {
-  let cityNumber = process.argv.indexOf(program.city);
+let flags = {
+  city: 'chicago',
+  country: 'us',
+  scale: 'f',
+};
 
-  weather.getWeather(process.argv[cityNumber]).then(res => {
-    console.log(res);
+if (program.city) {
+  let cityIndex = process.argv.indexOf(program.city);
+  flags.city = process.argv.slice(cityIndex).toString();
+}
+if (program.country) {
+  let countryIndex = process.argv.indexOf(program.country);
+  flags.country = process.argv.slice(countryIndex).toString();
+}
+if (program.scale) {
+  let scaleIndex = process.argv.indexOf(program.scale);
+  flags.scale = process.argv.slice(scaleIndex).toString();
+}
+
+if (program.forecast) {
+} else {
+  weather.getWeather(flags).then(res => {
+    console.log(chalk.red(`${res.city}, ${res.country}`));
+    console.log(chalk.cyan(`Condition: ${chalk.yellow(res.condition)}`));
+    console.log(chalk.cyan(`Temperature: ${chalk.yellow(res.temperature)}`));
   });
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
-// const apiKey = require('./secret');
 
 const API_URL_CITY = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const API_URL_FORECAST = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 const apiKey = 'b31d089c7cf4494d9ce4ded2a6bfb0f4';
 
 const toFahrenheit = temp => Math.round((9 / 5) * (temp - 273) + 32);
@@ -10,20 +10,25 @@ const toCelsius = temp => Math.round((temp - 32) * 0.5556);
 module.exports = {
   getWeather: flags => {
     return new Promise((resolve, reject) => {
-      let city = flags;
+      let city = flags.city;
+      let country = flags.country;
+      let scale = flags.scale;
       if (!city) {
         return new Error('City missing!');
       }
       axios
-        .get(`${API_URL_CITY}${city},us&appid=${apiKey}`)
+        .get(`${API_URL_CITY}${city},${country}&appid=${apiKey}`)
         .then(res => {
           let temp = toFahrenheit(res.data.main.temp);
           let result = Object.assign({
             city: res.data.name,
             country: res.data.sys.country,
-            Condition: res.data.weather[0].description,
-            Temperature: temp + ' F',
+            condition: res.data.weather[0].description,
+            temperature: temp + '°F',
           });
+          if (scale === 'c') {
+            result.temperature = toCelsius(temp) + '°C';
+          }
           resolve(result);
         })
         .catch(error => {
